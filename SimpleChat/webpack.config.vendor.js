6cf44a59202801267f24ae9e4100db3d3654f9b5
Maksim Sharonov <sharonovmi@gmail.com>
2017-10-08 18:52:16 +0700

    Fix UglifyJs issue

--- SimpleChat/webpack.config.vendor.js
+++ SimpleChat/webpack.config.vendor.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const merge = require('webpack-merge');
